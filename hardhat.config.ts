d8ff5eb36caab67702d1ee801d601b921ad70f4d
Fabiano Nascimento <fabianorodrigo@gmail.com>
2022-07-20 19:30:29 -0300

    Coverage, gasReporter and mocha-junit-reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,23 @@
-import { HardhatUserConfig } from "hardhat/config";
+import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "solidity-coverage";
+import "hardhat-gas-reporter";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  mocha: {
+    reporter: "mocha-multi-reporters",
+    reporterOptions: {
+      configFile: "./mocha-reporter-config.json",
+    },
+  },
+  gasReporter: {
+    currency: "EUR",
+    coinmarketcap: process.env.COINMARKETCAP_API_TOKEN,
+    showTimeSpent: true,
+    //outputFile: "reports/eth-gas-reporter.txt",
+    enabled: true,
+  },
 };
 
 export default config;
