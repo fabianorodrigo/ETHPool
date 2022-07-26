ba3a0c0c8f6fa33be1232838a6246974a6517497
Fabiano Nascimento <fabianorodrigo@gmail.com>
2022-07-26 19:15:41 -0300

    ETHPool: deploy and verify script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,16 +1,57 @@
-import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "solidity-coverage";
 import "@openzeppelin/hardhat-upgrades";
+import {HardhatUserConfig} from "hardhat/config";
+import "solidity-coverage";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+// RINKEBY API KEY
+const RINKEBY_ALCHEMY_API_KEY_URL = getEV("RINKEBY_ALCHEMY_API_KEY_URL");
+//ADMIN PRIVATE KEY
+const ADMIN_PRIVATE_KEY = getEV("ADMIN_PRIVATE_KEY");
+//MANAGER PRIVATE KEY
+const MANAGER_PRIVATE_KEY = getEV("MANAGER_PRIVATE_KEY");
+//TEAMMEMBER PRIVATE KEY
+const TEAMMEMBER_PRIVATE_KEY = getEV("TEAMMEMBER_PRIVATE_KEY");
+// ETHERSCAN API KEY
+const ETHERSCAN_API_KEY = getEV("ETHERSCAN_API_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: RINKEBY_ALCHEMY_API_KEY_URL,
+      accounts: [
+        ADMIN_PRIVATE_KEY,
+        MANAGER_PRIVATE_KEY,
+        TEAMMEMBER_PRIVATE_KEY,
+      ],
+    },
+  },
   mocha: {
     reporter: "mocha-multi-reporters",
     reporterOptions: {
       configFile: "./mocha-reporter-config.json",
     },
   },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
+
+/**
+ * Gets the environment variable value with the given name or throws an error if value is not set.
+ *
+ * @param name name of the environment variable
+ * @returns Value of the environment variable
+ */
+function getEV(name: string): string {
+  const value = process.env[name];
+  if (value == null || value.trim() == "") {
+    throw new Error(`Environment variable '${name}' is required`);
+  }
+  return value;
+}
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,17 +1,30 @@
-import {ethers} from "hardhat";
+import {ethers, run} from "hardhat";
+import * as dotenv from "dotenv";
+dotenv.config({path: ".env"});
+require("@nomiclabs/hardhat-etherscan");
 
 async function main() {
-  const [deployer, manager, teamMember] = await ethers.getSigners();
+  const [admin, manager, teamMember] = await ethers.getSigners();
   const ETHPoolFactory = await ethers.getContractFactory("ETHPool");
   const ethPool = await ETHPoolFactory.deploy(manager.address);
   await ethPool.deployed();
   console.log(`ETHPool deployed to '${ethPool.address}'`);
-  console.log(`Admin: '${deployer.address}'`);
+  console.log(`Admin: '${admin.address}'`);
   console.log(`Manager: '${manager.address}'`);
 
   const TEAM_ROLE = await ethPool.TEAM_ROLE();
-  await ethPool.connect(deployer).grantRole(TEAM_ROLE, teamMember.address);
+  await ethPool.connect(admin).grantRole(TEAM_ROLE, teamMember.address);
   console.log(`Team member: '${teamMember.address}'`);
+
+  console.log("Sleeping.....");
+  // Wait for etherscan to notice that the contract has been deployed
+  await sleep(60000);
+
+  // Verify the contract after deploying
+  await run("verify:verify", {
+    address: ethPool.address,
+    constructorArguments: [manager.address],
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -20,3 +33,7 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
